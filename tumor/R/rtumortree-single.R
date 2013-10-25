6628e94a0f008c3cca6751170c0b9ed9a0e35725
Thomas Mailund <mailund@birc.au.dk>
2013-10-25 17:59:35 +0200

    Fixed wrong name for coalescence time tree and added a bit of documentation.

--- tumor/R/rtumortree-single.R
+++ tumor/R/rtumortree-single.R
@@ -27,7 +27,9 @@ coaltimes.single <- function(n, delta, gamma) {
 #' Simulate a coalescence tree for case A in Wiuf's note. This is the genealogy
 #' of a single tumor where \code{n} cells were sampled.
 #' 
-#' 
+#' Coalescence times are simulated using \code{coaltimes.single} and the parameters
+#' of this function are the same as for that function. See the description of that function
+#' for a description of the parameters. The tree topology is random.
 #'
 #' @param n      The number of coalescence times / leaves.
 #' @param delta  Parameter for the birth/death process.
@@ -36,7 +38,7 @@ coaltimes.single <- function(n, delta, gamma) {
 #' @export
 rtumortree.single <- function(n, delta, gamma) {
   # Get time points from the birht/death process
-  ts <- birth.times(n, delta, gamma)
+  ts <- coaltimes.single(n, delta, gamma)
   
   # Coalescence times are the birth times from t2 and down to tn
   coal.times <- ts[-1]
@@ -46,7 +48,7 @@ rtumortree.single <- function(n, delta, gamma) {
   tree$ts <- ts
   
   # Define as a sub-class of "phylo" and return the new object
-  class(tree) <- c('tumortree', 'phylo')
+  class(tree) <- c('tumortree.single', 'tumortree', 'phylo')
   tree
 }
 
--- tumor/test.R
+++ tumor/test.R
@@ -1,7 +1,8 @@
 
 library(ape)
+
 opar <- par(mfrow=c(2,2))
-tree <- rtumortree(15, 1, 1)
+tree <- rtumortree.single(15, 1, 1)
 plot(tree)
 plot(mutate(tree, 0.01))
 plot(mutate(tree, 0.05))
