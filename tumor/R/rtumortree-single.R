0811d9f387cb290cdd0334ba1aec6d61a8be70f6
Thomas Mailund <mailund@birc.au.dk>
2013-10-25 18:14:21 +0200

    Moved function for simulating coalescence times for a single tumour to a separate file.
    This function will be used in simulation of other cases so shouldn't be a part of the single-tumour simulation file.

--- tumor/R/rtumortree-single.R
+++ tumor/R/rtumortree-single.R
@@ -1,27 +1,4 @@
 
-#' Simulate the coalescence times for a single tumor.
-#' 
-#' Simulate coalescence times for a tumor based on a birth/death process
-#' with birth rate lambda and death rate mu. The simulation uses
-#' scaled parameters delta = lambda - mu and gamma = (lambda-mu)/(lambda*rho)
-#' where rho is the fraction of tumor cells sequenced.
-#' 
-#' See Wiuf's note for details.
-#' 
-#' The function will return \code{n} time points, where t1 is the time for the 
-#' driver mutation and the remaining are the actual coalescence times for samples
-#' from the tumor.
-#' 
-#' @param n      The number of coalescence times.
-#' @param delta  Parameter for the birth/death process.
-#' @param gamma  Parameter for the birth/death process.
-#' 
-#' @export
-coaltimes.single <- function(n, delta, gamma) {
-  Us <- sort(runif(n))
-  -1.0/delta * log( Us / (gamma + (1-gamma) * Us) )
-}
-
 #' Simulate a tumor tree for a single tumor.
 #' 
 #' Simulate a coalescence tree for case A in Wiuf's note. This is the genealogy
