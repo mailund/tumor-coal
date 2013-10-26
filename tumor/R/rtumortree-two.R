32ffd39fceac3b4bedc18450105881e34aa7acb4
Thomas Mailund <mailund@birc.au.dk>
2013-10-26 07:30:45 +0200

    Fixed the math for sampling TA

---
+++ tumor/R/rtumortree-two.R
@@ -0,0 +1,44 @@
+
+#' Simulate a tumor tree for two tumours (case B).
+#' 
+#' Simulate a coalescence tree for case B in Wiuf's note. This is the genealogy
+#' of two single tumours sharing a single driver mutation.
+#' 
+#' The time for the divergence of the two tumours is sampled as T_A in Wiuf's note, and
+#' the two tumours are then sampled as single tumours in a similar way to 
+#' \code{coaltimes.single}, but offset by parameter tau. See \code{coaltimes.single}
+#' for details on how the individual tumour coalescence times are sampled.
+#' 
+#' @param n1      Number of samples / coalescence times in the first tumour.
+#' @param n2      Number of samples / coalescence times in the second tumour.
+#' @param delta   Parameter for the birth/death process.
+#' @param gamma1  Parameter for the birth/death process in the first tumour.
+#' @param gamma2  Parameter for the birth/death process in the second tumour.
+#' @param tau     Offset in time of the sampling of the two samples.
+#' 
+#' @export
+rtumortree.two <- function(n1, n2, delta, gamma1, gamma2, tau) {
+  # Get time points from the birth/death process in tumour 1
+  t1s <- coaltimes.single(n1, delta, gamma1)
+  # and then do the same for tumour 2
+  t2s <- coaltimes.single(n2, delta, gamma2)
+  
+  # Get the split time between the two tumours
+  gamma <- gamma1 + gamma2 # FIXME
+  TA <- twoTumors_CaseB_TA(n1, n2, delta, gamma, tau)
+  
+  # Simulate two coalescence trees as single tumours, with the respective coal times.
+  tree1 <- rcoal(n1, br = t1s[-1])
+  tree2 <- rcoal(n2, br = T2s[-1])
+  
+  # Merge the two trees at time T_A
+  tree <- bind(tree1, tree2, where = TA) # FIXME: WHERE ISN'T CORRECT
+  
+  tree$TA <- TA
+  tree$t1s <- t1s
+  tree$t2s <- t2s
+  
+  # Define as a sub-class of "phylo" and return the new object
+  class(tree) <- c('tumortree.two.B', 'tumortree', 'phylo')
+  tree
+}
--- tumor/src/caseB_T_A.cpp
+++ tumor/src/caseB_T_A.cpp
@@ -3,31 +3,32 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-double twoTumors_CaseB_TA(int n, double delta, double gamma, double tau) {
+double twoTumors_CaseB_TA(int n1, int n2, double delta, double gamma1, double gamma2, double tau) {
+    
     // Get x (see Wiuf's note)
     double x = 0.0;
-    if (gamma != 1.0) {
-        x = -n * gamma + sqrt((n*gamma)*(n*gamma) + 4*(1-gamma))/(2*(1-gamma));
+    if (gamma1 != 1.0) {
+        x = -n1 * gamma1 + sqrt((n1*gamma1)*(n1*gamma1) + 4*(1-gamma1))/(2*(1-gamma1));
     } else {
-        x = 1.0 / float(n);
+        x = 1.0 / float(n1);
     }
     
     // M -- see Wiuf's note.
-    double M = (x*pow(1-x, n-1))/pow(1-(1-gamma)*x, n+1);
+    double M = (x*pow(1-x, n1-1))/pow(1-(1-gamma2)*x, n1+1);
     
     // Rejection sampling following Wiuf's note.
     for (;;) {
         double t = 0.0;
         for (;;) {
             double U = runif(1, 0.0, 1.0)[0];
-            double X = 1/pow(U, 1.0/float(n)) - 1;
-            t = 1/delta * log(1 + gamma/X);
+            double X = 1/pow(U, 1.0/float(n2)) - 1;
+            t = 1/delta * log(1 + gamma2/X);
             double s = delta*(t - tau);
             if (s >= 0) break;
         }
         double accept = runif(1, 0.0, 1.0)[0];
-        double accept_prob_nom = (1.0/M) * exp(-t)*pow(1-exp(-t), n-1);
-        double accept_prob_denom = pow(1-(1-gamma)*exp(-t), n+1);
+        double accept_prob_nom = (1.0/M) * exp(-t)*pow(1-exp(-t), n1-1);
+        double accept_prob_denom = pow(1-(1-gamma1)*exp(-t), n1+1);
         double accept_prob = accept_prob_nom / accept_prob_denom;
         if (accept <= accept_prob)
             return t;
